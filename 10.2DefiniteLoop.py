fe70e205507829de3b245c02e4c93220671addc0
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-11-09 04:04:18 +0500

    10.2DefiniteLoop.py

---
+++ 10.2DefiniteLoop.py
@@ -0,0 +1,28 @@
+# for i in [5,4,e,2,1]:
+for i in [5,4,3,2,1]:
+    print(i)
+print("Done\n")
+
+friend = ["haseb", "khan", "Blah", "blah"]
+for i in friend:
+    print("Happy new year",i)
+print("Done!")
+
+# how many line it will print
+for i in [6,3,6,3]:
+    print(i)
+
+print("Before")
+for things in [6,3,6,3]:
+    print(things)
+print("After")
+
+    
+
+print("Before")
+largest = -1
+for things in [6,3,45,6,3]:
+    if things > largest:
+        largest = things
+print(largest)
+print("After")
