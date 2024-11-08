ea3205ad6127f9f2a338534aad8b22ca764ccae0
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-11-08 20:50:38 +0500

    Expresstions and Conditions

---
+++ 8ConditionalExecution.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+x = 3
+if (x == 10):
+    print(x)
+else:
+    print("Nothing")
+
+# Works same as if - else if - else
+
+if(x == 10):
+    print("First Codition")
+elif(x == 3):
+    print("Second Codition")
+elif(x == 6):
+    print("Thrid Codition")
