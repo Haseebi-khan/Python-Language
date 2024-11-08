786285d0352ecc24d8190471a68f8ffdf02e0086
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-11-09 00:49:23 +0500

    9.2 Function code

---
+++ 9.2Function.py
@@ -0,0 +1,27 @@
+# Simple calculator.
+def calculator(x,y):
+    inp = input("Enter operation: ")
+    if inp == '+':
+        suma = x + y
+        print("Sum is: ",suma)
+    elif inp == '*':
+        mul = x * y
+        print("Multiple is: ",mul)
+        
+    elif inp == '-':
+        sub = x - y
+        print("Minus is: ",sub)
+    elif inp == '/':
+        div = x / y
+        print("Div is: ",div)
+    else:
+        print("Enter coorect Operation.")
+    
+calculator(input("Enter first num: "),input("Enter Sec num: "))
+
+# simple func
+def nameFunc():
+    name = input("Enter your name: ")
+    return name
+
+print("Your name is: ",nameFunc())
