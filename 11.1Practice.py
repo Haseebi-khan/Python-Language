fbe116bbc8e857dcde34717198855c925b2f902a
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-11-10 00:20:45 +0500

    11.1Practice.py

--- 11.1Practice.py
+++ 11.1Practice.py
@@ -5,3 +5,24 @@ print(date.today())
 print("Today's date is: " , date.today())
 print("Today's date is: " + str(date.today()))
 # print(str(date.month()))
+
+# import numpy as np
+# np.arr([2,3,4,5])
+# print(n.arr)
+
+
+# Import pi function of math package
+from math import pi
+
+# Calculate C
+C = 2 * 0.43 * pi
+
+# Calculate A
+A = pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+
+import numpy
+print(numpy.__version__)
