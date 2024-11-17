4d17071be95816843883229b47a0ce076c1e72fd
Haseebe-Khan <haseebkhanbettani@gmail.com>
2024-11-17 19:29:54 +0500

    17.1.list

---
+++ 17.1.list.py
@@ -0,0 +1,16 @@
+fruit= [
+    "organ", "something", "blahblah", "apple"
+] 
+
+for item in fruit:
+    if item == "blahblah":
+        continue
+    else:
+        print(item)
+
+friends = ["A","B","C","D"]
+for i in range(len(friends)):
+    friend = friends[i]
+    print("Hi ", friend)
+     
+    
