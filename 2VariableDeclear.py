8128324945c1ad07caca683b32963f5dedcb16a3
haseeb khan <haseebkhanbettani@gmail.com>
2024-11-22 03:07:44 +0500

    minor change

--- 2VariableDeclear.py
+++ 2VariableDeclear.py
@@ -35,5 +35,10 @@ x = 'Hello' + ' ' + 'World!'
 print(x) # outputs: Hello World!
 
 
-
-print('Hello World!')
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+index = alphabet.find(text[0].lower())
+print(index)
+shifted = alphabet[index + shift]
+print(shifted)
