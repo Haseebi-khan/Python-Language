b4b2e3dd7fa40391c32950581bc2ed3203aeb896
haseeb khan <haseebkhanbettani@gmail.com>
2025-02-26 21:35:20 +0500

    Dictionary

---
+++ 1.5.Dictionary .py
@@ -0,0 +1,22 @@
+dic = {}
+dic1 = {1:'one', 2:'two'}
+dic[4]="This can be new Thing."
+dic["three"] = "This can also be new Thing."
+
+dict = {}
+dict['one'] = "This is one"
+dict[2]     = "This is two"
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+print (dict['one'])       # Prints value for 'one' key
+print (dict[2])           # Prints value for 2 key
+print (tinydict)          # Prints complete dictionary
+print (tinydict.keys())   # Prints all the keys
+print (tinydict.values()) # Prints all the values
+print("\n\n")
+print (dic.keys()) # Prints all the values
+print (dic.values()) # Prints all the values
+print("\n\n")
+
+print(dic)
