896e9d74c28cb81c8539040b20b1f7cc4f508ad3
haseeb khan <haseebkhanbettani@gmail.com>
2025-03-16 06:47:47 +0500

    Practice Questions.

--- StringAssigment/1.7.ReplaceingWords.py
+++ StringAssigment/1.7.ReplaceingWords.py
@@ -9,4 +9,29 @@
 # Expected Result : 'The lyrics is good!'
 # 'The lyrics is poor!'
 
+def ReplacingString(string):
+    
+    wordsList = string.split()
+    notFound = False
+    poorFound = False
+    
+    for i, word in enumerate(wordsList):
+        if word == 'not':
+            notIndex = i
+            notFound = True
+        if notFound and word.lower() == 'poor':
+            poorIndex = i
+            poorFound = True
+            break
+        
+    if notFound and poorFound and notIndex < poorIndex :
+        resultWords = wordsList[:notIndex] + ['good'] + wordsList[poorIndex+1:]
+        result = " ".join(resultWords)
+    else:
+        result = string 
+        
+    return result + "!"
+    
+result =  ReplacingString("The lyrics is not that poor")
 
+print(result)
