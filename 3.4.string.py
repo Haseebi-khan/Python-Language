cfac81e8e6443f8862f75e8a4aa1e50dccb3a619
haseeb khan <haseebkhanbettani@gmail.com>
2025-03-16 11:38:08 +0500

    String

---
+++ 3.4.string.py
@@ -0,0 +1,43 @@
+
+
+name = "!!!!!HaseEB KhaN!!!!! is"
+
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.rstrip("!"))
+print(name.replace("HaseEB KhaN","HASEEB KHAN"))
+newName = name
+print(newName.split())
+print(name.capitalize())
+print(len(name))
+print(name.center(50))
+print(len(name.center(50)))
+
+print(name.count("a"))
+
+print(name.endswith("Khan" or "KhaN!!!!!" or "!!!!!" or 'is'))
+print(name.find("is"))
+print(name)
+string = "WellCometoConsole"
+string2 = "Well Cometo Console"
+print(string.isalnum())
+print(string2.isalnum())
+print(string.isalpha())
+
+name2 = "Haseeb Khan"
+
+
+print(name2.istitle())
+print(name.istitle())
+print(name2.startswith("Haseeb"))
+print(name.swapcase())
+
+anotherName = "Haseeb Ullah Khan"
+anotherName = anotherName.split()
+
+hName = "Code With Mosh"
+hName.split()
+print(type(hName))
+
+print(type(anotherName))
